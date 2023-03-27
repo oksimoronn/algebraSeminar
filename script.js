71322998cc0eca3a39c8d3779b1c957effb070b9
oksimoronn <zvonetola@gmail.com>
2023-03-27 22:10:47 +0200

    popravljeno lijevo desno

--- script.js
+++ script.js
@@ -25,92 +25,89 @@ function getRandomColor() {
   return randColor;
 }
 
-function init() {
-  const CLIENT_ID = "wAHFjVc0vWlFPALd";
+const CLIENT_ID = "wAHFjVc0vWlFPALd";
 
-  function createMsg(nam, id, txt, col) {
-    const colorOfMesege = col;
+function createMsg(nam, id, txt, col) {
+  const colorOfMesege = col;
 
-    let membersId = id;
+  let membersId = id;
 
-    const mainWindow = document.querySelector(".msgWindow");
-    mainWindow.style.display = "flex";
+  const mainWindow = document.querySelector(".msgWindow");
+  mainWindow.style.display = "flex";
 
-    const msgContainer = document.createElement("div");
-    msgContainer.className = `msgTxt ${membersId}`;
-    mainWindow.appendChild(msgContainer);
+  const msgContainer = document.createElement("div");
+  msgContainer.className = `msgTxt ${membersId}`;
+  mainWindow.appendChild(msgContainer);
 
-    const msgSender = document.createElement("h3");
-    msgSender.innerText = nam;
-    msgSender.style.color = colorOfMesege;
-    msgContainer.appendChild(msgSender);
+  const msgSender = document.createElement("h3");
+  msgSender.innerText = nam;
+  msgSender.style.color = colorOfMesege;
+  msgContainer.appendChild(msgSender);
 
-    const msgText = document.createElement("p");
-    msgText.innerText = `: ${txt}`;
-    msgText.style.color = colorOfMesege;
-    msgContainer.appendChild(msgText);
-  }
-
-  const drone = new ScaleDrone(CLIENT_ID, {
-    data: {
-      name: getRandomName(),
-      color: getRandomColor(),
-    },
-  });
-
-  drone.on("open", () => {
-    console.log("Successfully connected to Scaledrone");
-
-    const room = drone.subscribe("observable-room");
-
-    room.on("open", () => {
-      console.log("Successfully joined room");
-    });
-
-    room.on("data", (text, member) => {
-      const color = member.clientData.color;
-
-      let membersId = member.id;
-
-      const sender = member.clientData.name;
-
-      const innerTxt = text;
-
-      createMsg(sender, membersId, innerTxt, color);
-    });
-  });
-
-  drone.on("close", () => {
-    window.location.replace("./homePage.html");
-  });
-
-  const close = document.querySelector(".closeFrm");
-  close.addEventListener("submit", (e) => {
-    e.preventDefault();
-    drone.close();
-  });
-
-  const messegeForm = document.querySelector(".msgForm");
-  const inputForm = document.querySelector(".message-form__input");
-  messegeForm.addEventListener("submit", () => {
-    const msgTextValue = inputForm.value;
-
-    const checkIput = msgTextValue.trim();
-
-    if (checkIput == "") {
-      inputForm.value = "";
-      return;
-    } else {
-      inputForm.value = "";
-
-      drone.publish({
-        room: "observable-room",
-        message: msgTextValue,
-      });
-    }
-  });
+  const msgText = document.createElement("p");
+  msgText.innerText = `: ${txt}`;
+  msgText.style.color = colorOfMesege;
+  msgContainer.appendChild(msgText);
 }
-init();
+
+const drone = new ScaleDrone(CLIENT_ID, {
+  data: {
+    name: getRandomName(),
+    color: getRandomColor(),
+  },
+});
+
+drone.on("open", () => {
+  //console.log("Successfully connected to Scaledrone");
+
+  const room = drone.subscribe("observable-room");
+
+  room.on("open", () => {
+    //console.log("Successfully joined room");
+  });
+
+  room.on("data", (text, member) => {
+    const color = member.clientData.color;
+
+    let membersId = member.id;
+
+    const sender = member.clientData.name;
+
+    const innerTxt = text;
+
+    createMsg(sender, membersId, innerTxt, color);
+  });
+});
+
+drone.on("close", () => {
+  window.location.replace("./homePage.html");
+});
+
+const close = document.querySelector(".closeFrm");
+close.addEventListener("submit", (e) => {
+  e.preventDefault();
+  drone.close();
+});
+
+const messegeForm = document.querySelector(".msgForm");
+const inputForm = document.querySelector(".message-form__input");
+messegeForm.addEventListener("submit", () => {
+  const msgTextValue = inputForm.value;
+
+  const checkIput = msgTextValue.trim();
+
+  if (checkIput == "") {
+    inputForm.value = "";
+    return;
+  } else {
+    inputForm.value = "";
+
+    drone.publish({
+      room: "observable-room",
+      message: msgTextValue,
+    });
+  }
+});
 
 const targetNode = document.querySelector(".msgWindow");
 
@@ -119,14 +116,13 @@ const config = { childList: true };
 const callback = (mutationList, observer) => {
   for (const mutation of mutationList) {
     if (mutation.type === "childList") {
+      const firstUserClassName = drone.clientId;
+
       const msgElements = document.querySelectorAll(".msgTxt");
       const nmbrOfMeseges = msgElements.length;
-      console.log(msgElements, nmbrOfMeseges);
-
-      msgElements[0].setAttribute("style", "margin-left:-30vw");
 
       for (let i = 0; i < nmbrOfMeseges; i++) {
-        if (msgElements[0].classList[1] !== msgElements[i].classList[1]) {
+        if (firstUserClassName !== msgElements[i].classList[1]) {
           msgElements[i].setAttribute("style", "margin-right:-30vw");
           msgElements[i].scrollIntoView({ behavior: "smooth" });
         } else {
